6b8283c813971cba64568a363856a37b75252003
valeria <a.valeria.koriatchenko@freenet.de>
2018-04-24 17:00:55 +0200

    Improve classes

--- js/app.js
+++ js/app.js
@@ -1,47 +1,62 @@
 const allEnemies = []
 let pointsCounter = 0;
 
-var Enemy = function(x,y) {
-    this.x = x
-    this.y = y
-    this.sprite = 'images/enemy-bug.png';
-};
+let pointsSpan = document.querySelector(".scorePoints")
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
- 
-    if (this.x < 500) {
-        (this.x += randomSpeed())*dt
-    } else {
-        this.x = 0
-        this.y = randomRow()
+class Element {
+    constructor(x,y,sprite) {
+        this.x = x
+        this.y = y
+        this.sprite = sprite
     }
-};
+
+    render() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    }
+}
+
+
+class Enemy extends Element {
+    constructor(x,y,sprite) {
+        super (x,y,sprite)
+    }
+
+    update(dt) {
+ 
+        if (this.x < 500) {
+            (this.x += randomSpeed())*dt
+        } else {
+            this.x = random_x()
+            this.y = random_y()
+        }
+    };
+}
+
+(function instantiateEnemies() {
+    for (let i = 0; i < 4; i++) {
+        var bug = new Enemy(random_x(),random_y(),'images/enemy-bug.png')
+        allEnemies.push(bug)
+    }
+})();
 
 // Make bugs appear on random stone row
-function randomRow(){
+function random_y(){
     let possibleRow = [219, 136, 53]
     let randRow = Math.floor(Math.random() * possibleRow.length)
     return possibleRow[randRow]
 }
 
+//Make bugs appear at random distance left from canvas
+function random_x(){
+    let random_xPosition = Math.floor(Math.random() * ((-50)+200)-200)
+    return random_xPosition
+}
+
+//Give bugs a random speed
 function randomSpeed(){
     let randomSpeedVar = Math.floor(Math.random() * (4-2)+2)
     return randomSpeedVar
 }
-
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-
-const enemy1 = new Enemy(-101,randomRow())
-const enemy2 = new Enemy(-202,randomRow())
-const enemy3 = new Enemy(-350,randomRow())
-allEnemies.push(enemy1)
-allEnemies.push(enemy2)
-allEnemies.push(enemy3)
 
 
 // Check Collision
@@ -53,39 +68,67 @@ function checkCollision(ent1,ent2) {
     return distance < 50
 }
 
-const Player = function(x,y) {
-    this.x = x
-    this.y = y
-    this.sprite = 'images/char-cat-girl.png'
+class Player extends Element {
+    constructor(x,y,sprite) {
+        super (x,y,sprite)
+        this.lifes = 3
+        this.points = 0
+    }
+
+    reset_y() {
+        this.y = 405
+    }
+
+    addPoints() {
+        this.points += 8
+    }
 }
 
-var player = new Player(202,405); //202,405
+var player = new Player(202,405,'images/char-cat-girl.png'); //202,405
 
-Player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y)
+
+Player.prototype.update = function() {
+
+    // for (let i = 0; i < allEnemies.length; i++) {
+    //     var vx = ent1.x - ent2.x
+    //     var vy = ent1.y - ent2.y
+    //     var distance = Math.sqrt(vx*vx+vy*vy)
+            
+    //     return distance < 50
+    // }
+
+    
 }
-
-Player.prototype.update = function(){}
 
 Player.prototype.handleInput = function(key){
 
-    if (key == 'up') {
-        if (this.y === 73) {    // player --> Water
-            this.y = 405        // Reset player to grass 
-        } else {this.y -= 83}
-    } else if (key == 'left') {
-        if (this.x < 25) {
-            return
-        } else this.x -= 100
-    } else if (key == 'down') {
-        if (this.y > 404) {
-            return
-        } else this.y += 83
-    } else if (key == 'right') {
-        if (this.x > 400) {
-            return
-        } else this.x += 100
+    switch(key) {
+        case ('up'):
+            if (this.y === 73) {    // player --> Water
+                this.reset_y()     // Reset player to grass 
+                this.addPoints()
+            } else {this.y -= 83}
+            break;
+
+        case ('left'):
+            if (this.x < 25) {
+                return
+            } else this.x -= 100
+            break;
+
+        case ('down'):
+            if (this.y > 404) {
+                return
+            } else this.y += 83
+            break;
+
+        case ('right'): 
+            if (this.x > 400) {
+                return
+            } else this.x += 100
+            break;
     }
+    
 }
 
 
